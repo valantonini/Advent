24809e6104d052003f794abe939688d9b4eedd15
val antonini <val.antonini@deswik.com>
2021-01-14 07:50:36 +1000

    Basic player controls with wasd

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
-import {GameLoop, init, Sprite} from 'kontra';
+import {GameLoop, init, Sprite, initKeys, keyPressed} from 'kontra';
 
-let { canvas, context } = init();
+const {canvas} = init();
 
-let sprite = Sprite({
+initKeys();
+
+const sprite = Sprite({
     x: 100,        // starting x,y position of the sprite
     y: 80,
     color: 'red',  // fill color of the sprite rectangle
     width: 20,     // width and height of the sprite rectangle
     height: 40,
-    dx: 2          // move the sprite 2px to the right every frame
+    //dx: 2          // move the sprite 2px to the right every frame
 });
 
 let loop = GameLoop({  // create the main game loop
-    update: function() { // update the game state
-        sprite.update();
+    update: function () { // update the game state
 
-        // wrap the sprites position when it reaches
-        // the edge of the screen
-        if (sprite.x > canvas.width) {
-            sprite.x = -sprite.width;
+
+        const playerMovementSpeed = 4;
+        if (keyPressed('a')) {
+            if (sprite.x - playerMovementSpeed >= 0) {
+                sprite.x -= playerMovementSpeed;
+            }
         }
+
+        if (keyPressed('d')) {
+            if ((sprite.x + sprite.width) + playerMovementSpeed <= canvas.width) {
+                sprite.x += playerMovementSpeed;
+            }
+        }
+
+        if (keyPressed('w')) {
+            if (sprite.y - playerMovementSpeed >= 0) {
+                sprite.y -= playerMovementSpeed;
+            }
+        }
+
+        if (keyPressed('s')) {
+            if ((sprite.y + sprite.height) + playerMovementSpeed <= canvas.height + 2) {
+                sprite.y += playerMovementSpeed;
+            }
+        }
+
+        sprite.update();
     },
-    render: function() { // render the game state
+    render: function () { // render the game state
         sprite.render();
     }
 });
